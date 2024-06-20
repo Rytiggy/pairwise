6250aef3ee64bd0649657315a56a7226fdf5e074
Ryan Mason <ryanmasonjar@gmail.com>
2024-06-19 23:27:42 -0400

    Update create.vue

--- src/pages/create.vue
+++ src/pages/create.vue
@@ -1,9 +1,10 @@
 <script setup>
 import { ref } from "vue";
 import btn from "../components/btn.vue";
+import { useQuestions } from "../composables/useQuestions";
 const title = ref("");
 const body = ref("");
-
+const { createQuestion } = useQuestions();
 function createQuestionPair() {
   console.log({ title, body });
   createQuestion(title, body);
